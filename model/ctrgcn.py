ca0e0fab4c44810e393c059f6303b612a950131d
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2023-12-27 20:58:13 +0700

    order on feature space

--- model/ctrgcn.py
+++ model/ctrgcn.py
@@ -215,8 +215,8 @@ class Model(nn.Module):
         
         print(start_frames)
         print(feat_fin[i, :, start_frames[0, 0] : start_frames[0, 0] + window, :].shape)
-        clip1 = torch.tensor([feat_fin[i, :, start_frames[i, 0] : start_frames[i, 0] + window, :] for i in range(feat_fin.shape[0])])
-        clip2 = torch.tensor([feat_fin[i, :, start_frames[i, 1] : start_frames[i, 1] + window, :] for i in range(feat_fin.shape[0])])
+        clip1 = torch.stack([feat_fin[i, :, start_frames[i, 0] : start_frames[i, 0] + window, :] for i in range(feat_fin.shape[0])])
+        clip2 = torch.stack([feat_fin[i, :, start_frames[i, 1] : start_frames[i, 1] + window, :] for i in range(feat_fin.shape[0])])
 
         print("Clip shape:", clip1.shape, clip2.shape)
         clips = torch.cat((clip1, clip2))
