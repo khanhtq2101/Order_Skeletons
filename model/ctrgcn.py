31e02f9b0b8da548809ef0023fac47c0b1017764
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2024-01-12 16:53:37 +0700

    coding

--- model/ctrgcn.py
+++ model/ctrgcn.py
@@ -182,8 +182,10 @@ class Model(nn.Module):
         if order_label is not None:
             order_clip =  self.order_clip_sampling(x, M, order_label)
             order_feat = self.forward_backbone(order_clip)
+            self.order_head()
 
         print("Order clip shape", order_clip.shape)
+        print("Order feature shape:", order_feat.shape)
 
         x = self.forward_backbone(x)
         feat_fin = x.clone() # 2N*M, 4C, T/4, V
@@ -205,20 +207,8 @@ class Model(nn.Module):
     def order_clip_sampling(self, feat_fin, M, order_label):
         N, C, T, V, M = feat_fin.size()
 
-        #print("Before:", feat_fin.shape)
-        #feat_fin = feat_fin.view(-1, M, C, T, V)
-
-        #print("After:", feat_fin.shape)
-        
-        #feat_fin = feat_fin.mean(1) #mean person
-        
-        #print("After mean:", feat_fin.shape)
-        #print("Order label shape:", order_label.shape)
-        #print("Order label", order_label)
-
         window = self.window_feature
         start_frames = np.zeros((feat_fin.shape[0], 2), dtype = int)
-        #print(start_frames.shape)
 
         for i, label in enumerate(order_label):
             start_frame = np.random.choice(T - window, size = 2, replace = False)
@@ -229,18 +219,12 @@ class Model(nn.Module):
 
             if label.item():
                 start_frame = np.flip(start_frame)
-            #print("Start frames:", i, start_frame, label.item())
             start_frames[i, :] = start_frame
         
-        #print(start_frames)
-        #print(feat_fin[i, :, start_frames[0, 0] : start_frames[0, 0] + window, :].shape)
         clip1 = torch.stack([feat_fin[i, :, start_frames[i, 0] : start_frames[i, 0] + window, :, :] for i in range(feat_fin.shape[0])])
         clip2 = torch.stack([feat_fin[i, :, start_frames[i, 1] : start_frames[i, 1] + window, :, :] for i in range(feat_fin.shape[0])])
 
-        #print("Clip shape:", clip1.shape, clip2.shape)
         clips_feat_fin = torch.cat((clip1, clip2))
-
-        #print("Concatenate shape:", clips_feat_fin.shape)
 
         return clips_feat_fin
 
