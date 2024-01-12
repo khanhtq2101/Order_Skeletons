2e36aa5c1396e0618202b520b51ec2efd41e21a3
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2024-01-12 17:07:25 +0700

    coding

--- main.py
+++ main.py
@@ -301,11 +301,9 @@ class Processor:
             self.global_step += 1
             
             #for order on clip space
-            B, C, T, V, M= data.shape
+            #B, C, T, V, M= data.shape
             #data = data.view(2*B, int(C/2), T, V, M)
-            #label = torch.flatten(label)
-            if batch_idx == 1:
-                data = data.view(2*B, C, int(T/2), V, M)
+            #label = torch.flatten(label)           
 
             print("Batch {} data shape:".format(batch_idx), data.shape)
 
